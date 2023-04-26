660dfe287214f98c24a5245c6e6dfa336ff94d08
Asilsay <lamufaku@gmail.com>
2023-04-27 02:02:46 +0700

    style: slicing myEvent

--- src/pages/MyEvent.tsx
+++ src/pages/MyEvent.tsx
@@ -17,7 +17,8 @@ interface HomeCap {
 const Home: FC = () => {
   const [datas, setDatas] = useState<HomeCap[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [tabValue, setTabValue] = useState(0);
+  const [tabValue, setTabValue] = useState(1);
+  const [isActive, setIsActive] = useState(true);
 
   useEffect(() => {
     fetchData();
@@ -45,6 +46,78 @@ const Home: FC = () => {
       <div className="max-h-[200vh] py-12 flex flex-col items-center w-full">
         <p className="font-['Lexend_Deca'] text-6xl text-white">My Event</p>
         <hr className="w-[60%] h-1 mx-auto my-6 bg-[#20DF7F] border-0 rounded " />
+        <div data-theme="mytheme" className="tabs tabs-boxed gap-9 bg-inherit">
+          <a
+            className={`${tabValue === 0 ? "tab-active" : ""} tab tab-lg`}
+            onClick={() => setTabValue(0)}
+          >
+            Joined Event
+          </a>
+          <a
+            className={`${tabValue === 1 ? "tab-active" : ""} tab tab-lg`}
+            onClick={() => setTabValue(1)}
+          >
+            Hosted Event
+          </a>
+          <a
+            className={`${
+              tabValue === 2 ? "tab-active" : ""
+            } tab tab-lg tab-disabled`}
+          >
+            Past Event
+          </a>
+        </div>
+
+        <div className="flex justify-center">
+          {tabValue === 0 ? (
+            <div className="w-[55%] mt-3 grid gap-3 grid-cols-1 overflow-auto">
+              {loading ? (
+                <Spinner />
+              ) : (
+                datas.map((data, idx) => {
+                  return (
+                    <CardHome
+                      key={data.id}
+                      date={data.date}
+                      host_name={data.host_name}
+                      image={data.image}
+                      name={data.name}
+                      quota={data.quota}
+                    />
+                  );
+                })
+              )}
+            </div>
+          ) : tabValue === 1 ? (
+            <div className="w-[55%] mt-3 grid gap-3 grid-cols-1 overflow-auto">
+              {loading ? (
+                <Spinner />
+              ) : (
+                datas.map((data, idx) => {
+                  return (
+                    <CardHome
+                      key={data.id}
+                      date={data.date}
+                      host_name={data.host_name}
+                      image={data.image}
+                      name={data.name}
+                      quota={data.quota}
+                    />
+                  );
+                })
+              )}
+              <CardHome
+                date="2023-05-01T10:00:00Z"
+                host_name="Jakarta Media Post"
+                image="/defaultImg.png"
+                name="Webinar: Level Up Your Business Through SEO"
+                quota={40}
+              />
+            </div>
+          ) : (
+            <p>NONE</p>
+          )}
+        </div>
       </div>
     </Layout>
   );
