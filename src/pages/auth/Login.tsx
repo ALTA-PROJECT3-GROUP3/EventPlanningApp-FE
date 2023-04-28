2e44472aa1890794b90c0d67f5b624ab82bea0a3
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-04-28 11:07:53 +0700

    style: slicing login

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,7 +1,52 @@
 import { FC } from "react";
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  return (
+    <div className="bg-[#093545]">
+      <div className="relative flex flex-col justify-center min-h-screen">
+        <div className="w-[25%] p-6 m-auto bg-[#093545] rounded-md shadow-xl">
+          <h1 className="text-7xl font-extrabold text-center text-white">
+            Sign in
+          </h1>
+          <p className="text-lg font-semibold text-center text-black-500 mt-7">
+            Sign in and join an event do you want!
+          </p>
+          <form className="mt-20 flex flex-col justify-center">
+            <div>
+              <input
+                type="text"
+                placeholder="Username"
+                className="block w-full px-4 py-3 text-white bg-[#224957] border rounded-lg focus:outline-none border-none mb-5"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                className="block w-full px-4 py-3 text-white bg-[#224957] border rounded-lg focus:outline-none border-none"
+              />
+            </div>
+            <div className="w-full">
+              <button
+                type="submit"
+                className="w-full py-3 mt-20 tracking-wide text-white transition-colors duration-200 transform bg-green-600 rounded-xl shadow-xl hover:bg-green-800 focus:outline-none focus:bg-black-600"
+              >
+                Sign In
+              </button>
+              <p className="text-center font-medium text-white pt-6">
+                Don't have an account?{" "}
+                {/* {
+                  <Link className=" text-library-logo" to={"/register"}>
+                    Sign up
+                  </Link>
+                } */}
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
