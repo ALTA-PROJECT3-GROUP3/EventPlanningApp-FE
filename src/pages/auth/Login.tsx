1f177280e71db7e8685e4d5c318f9c0a0732a1a3
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-05-04 08:55:17 +0700

    feat: integrating axios to login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,6 +1,46 @@
 import { FC } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login: FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setUsername(e.currentTarget.value);
+  };
+
+  const handlePasswordChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .post(
+        "https://virtserver.swaggerhub.com/CW3-ALTA/EventPlanningApp/1.0.0/login",
+        {
+          username,
+          password,
+        }
+      )
+      .then((response) => {
+        const data = response.data;
+        // Cookies.set("userData", JSON.stringify(data));
+        // const userData = Cookies.get("userData");
+        console.log(data);
+        // console.log(userData);
+
+        // navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="bg-[#093545]">
       <div className="relative flex flex-col justify-center min-h-screen">
@@ -11,12 +51,17 @@ const Login: FC = () => {
           <p className="text-lg font-semibold text-center text-black-500 mt-7">
             Sign in and join an event do you want!
           </p>
-          <form className="mt-20 flex flex-col justify-center">
+          <form
+            className="mt-20 flex flex-col justify-center"
+            onSubmit={handleLogin}
+          >
             <div>
               <input
                 type="text"
                 placeholder="Username"
                 className="block w-full px-4 py-3 text-white bg-[#224957] border rounded-lg focus:outline-none border-none mb-5"
+                value={username}
+                onChange={handleUsernameChange}
               />
             </div>
             <div>
@@ -24,6 +69,8 @@ const Login: FC = () => {
                 type="password"
                 placeholder="Password"
                 className="block w-full px-4 py-3 text-white bg-[#224957] border rounded-lg focus:outline-none border-none"
+                value={password}
+                onChange={handlePasswordChange}
               />
             </div>
             <div className="w-full">
@@ -35,11 +82,14 @@ const Login: FC = () => {
               </button>
               <p className="text-center font-medium text-white pt-6">
                 Don't have an account?{" "}
-                {/* {
-                  <Link className=" text-library-logo" to={"/register"}>
+                {
+                  <Link
+                    className="text-[#20df7f] text-md font-semibold"
+                    to={"/u/:username/"}
+                  >
                     Sign up
                   </Link>
-                } */}
+                }
               </p>
             </div>
           </form>
